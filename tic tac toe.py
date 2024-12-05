eb281d7d3ae04a63852194f8df4ebe400f150235
Tamjidthecoder <tamjidcool797@gmail.com>
2024-12-06 00:22:27 +0600

    Add files via upload

---
+++ tic tac toe.py
@@ -0,0 +1,69 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-"*9)
+
+def check_winner(board):
+    for i in range(3):
+        if board[i][0]==board[i][1]==board[i][2]!=" ":
+            return board[i][0]
+        if board[0][i]==board[1][i]==board[2][i]!=" ":
+            return board[0][i]
+    if board[0][0]==board[1][1]==board[2][2]!=" ":
+            return board[0][0]
+    if board[0][2]==board[1][1]==board[2][0]!=" ":
+            return board[0][2]
+    return None
+
+def is_full(board):
+    return all(cell !=" " for row in board for cell in row)
+
+
+
+
+
+
+
+
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3) ]
+    current_player="X"
+    while True:
+        print_board(board)
+        try:
+            row=int(input(f"player {current_player} ,enter the row(0-2): "))
+            col=int(input(f"player {current_player} ,enter the column(0-2): "))
+            if board[row][col]==" ":
+                board[row][col]=current_player
+                winner=check_winner(board)
+                if winner:
+                    print_board(board)
+                    print(f"player {winner} wins!")
+                elif is_full(board):
+                    print_board(board)
+                    print("draw")
+                    break
+                current_player="O" if current_player=="X" else "x"
+            else:
+                print("Here take a chocllate!pleas play agn")
+        except(ValueError,IndexError):
+            print("invalid!ENTER NUMBERS BETWEEN 0 - 2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
+    
